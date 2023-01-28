6c3c5d6cdfbb76d76c9d0f4016224d7a9db61c83
lucasbear <rukazu89@gmail.com>
2023-01-27 22:44:38 -0600

    Project Setup

---
+++ src/main/java/com/lubear/todoapp/context/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package com.lubear.todoapp.context;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+}
